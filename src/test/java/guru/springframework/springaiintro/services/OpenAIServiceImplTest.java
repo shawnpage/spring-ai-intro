1967cfd68b2e637442a1b3056ded398cc8dfa200
Shawn Page <shawn.page@gmail.com>
2026-03-23 09:51:26 -0400

    Added     Question, Answer, GetCapitalRequest records     get-capital-prompt string template     OpenAIService and OpenAIServiceImpl to services     getAnswer, getCapital to OpenAIServiceImpl     OpenAIServiceImplTest     QuestionController

---
+++ src/test/java/guru/springframework/springaiintro/services/OpenAIServiceImplTest.java
@@ -0,0 +1,18 @@
+package guru.springframework.springaiintro.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenAIServiceImplTest {
+
+    @Autowired
+    OpenAIServiceImpl openAIService;
+
+    @Test
+    void getAnswer() {
+        String answer = openAIService.getAnswer("What is the capital of France?");
+        System.out.println(answer);
+    }
+}
