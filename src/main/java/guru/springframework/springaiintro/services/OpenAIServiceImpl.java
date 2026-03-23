75d6f9b33af53195974bab11683eb353cd427d82
Shawn Page <shawn.page@gmail.com>
2026-03-23 10:52:07 -0400

    Modified the getCapital method to return JSON.

--- src/main/java/guru/springframework/springaiintro/services/OpenAIServiceImpl.java
+++ src/main/java/guru/springframework/springaiintro/services/OpenAIServiceImpl.java
@@ -1,5 +1,9 @@
 package guru.springframework.springaiintro.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import guru.springframework.springaiintro.model.Answer;
 import guru.springframework.springaiintro.model.GetCapitalRequest;
 import guru.springframework.springaiintro.model.Question;
@@ -7,6 +11,7 @@ import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
@@ -21,6 +26,15 @@ public class OpenAIServiceImpl implements OpenAIService {
     public OpenAIServiceImpl(ChatModel chatModel) {
         this.chatModel = chatModel;
     }
+
+    @Value("classpath:templates/get-capital-prompt.st")
+    private Resource getCapitalPrompt;
+
+//    @Value("classpath:templates/get-capital-with-info.st")
+//    private Resource getCapitalPromptWithInfo;
+
+    @Autowired
+    ObjectMapper objectMapper;
 
     @Override
     public Answer getAnswer(Question question) {
@@ -41,20 +55,25 @@ public class OpenAIServiceImpl implements OpenAIService {
         return new Answer(response.getResult().getOutput().getText());
     }
 
-    @Value("classpath:templates/get-capital-prompt.st")
-    private Resource getCapitalPrompt;
-
-    @Value("classpath:templates/get-capital-with-info.st")
-    private Resource getCapitalPromptWithInfo;
-
     @Override
     public Answer getCapital(GetCapitalRequest getCapitalRequest) {
-//        PromptTemplate promptTemplate = new PromptTemplate(getCapitalPrompt);
-        PromptTemplate promptTemplate = new PromptTemplate(getCapitalPromptWithInfo);
+        PromptTemplate promptTemplate = new PromptTemplate(getCapitalPrompt);
         Prompt prompt = promptTemplate.create(Map.of("stateOrCountry", getCapitalRequest.stateOrCountry()));
 
         ChatResponse response = chatModel.call(prompt);
-        return new Answer(response.getResult().getOutput().getText());
+
+        System.out.println(response.getResult().getOutput().getText());
+        String responseString;
+
+        try {
+            JsonNode jsonNode = objectMapper.readTree(response.getResult().getOutput().getText());
+            responseString = jsonNode.get("answer").asText();
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+//        return new Answer(response.getResult().getOutput().getText());
+        return new Answer(responseString);
     }
 
     @Override
